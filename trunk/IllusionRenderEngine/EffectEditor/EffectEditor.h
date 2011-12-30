57faba9856b26ac2e1feadd17b18efaa69f15e1f
dingliang1989 <dingliang1989@77eebbda-64c4-3d00-676f-254c11389491>
2011-12-30 07:51:10 +0000

    工程创建--特效编辑器

---
+++ trunk/IllusionRenderEngine/EffectEditor/EffectEditor.h
@@ -0,0 +1,32 @@
+
+// EffectEditor.h : EffectEditor 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CEffectEditorApp:
+// 有关此类的实现，请参阅 EffectEditor.cpp
+//
+
+class CEffectEditorApp : public CWinApp
+{
+public:
+	CEffectEditorApp();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CEffectEditorApp theApp;
