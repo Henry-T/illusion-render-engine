b36a9dd331733c6851193c1dc5016a0c1b677224
dingliang1989 <dingliang1989@77eebbda-64c4-3d00-676f-254c11389491>
2011-12-30 07:48:50 +0000

    工程创建--地图编辑器

---
+++ trunk/IllusionRenderEngine/WorldEditor/WorldEditorView.h
@@ -0,0 +1,46 @@
+
+// WorldEditorView.h : CWorldEditorView 类的接口
+//
+
+#pragma once
+
+
+class CWorldEditorView : public CView
+{
+protected: // 仅从序列化创建
+	CWorldEditorView();
+	DECLARE_DYNCREATE(CWorldEditorView)
+
+// 特性
+public:
+	CWorldEditorDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CWorldEditorView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // WorldEditorView.cpp 中的调试版本
+inline CWorldEditorDoc* CWorldEditorView::GetDocument() const
+   { return reinterpret_cast<CWorldEditorDoc*>(m_pDocument); }
+#endif
+
