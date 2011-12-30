667a462ca3b5499f4f0cfcfc58b24d1f73e33fa8
dingliang1989 <dingliang1989@77eebbda-64c4-3d00-676f-254c11389491>
2011-12-30 07:48:50 +0000

    工程创建--地图编辑器

---
+++ IllusionRenderEngine/WorldEditor/WorldEditorDoc.cpp
@@ -0,0 +1,137 @@
+
+// WorldEditorDoc.cpp : CWorldEditorDoc 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "WorldEditor.h"
+#endif
+
+#include "WorldEditorDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CWorldEditorDoc
+
+IMPLEMENT_DYNCREATE(CWorldEditorDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CWorldEditorDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CWorldEditorDoc 构造/析构
+
+CWorldEditorDoc::CWorldEditorDoc()
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CWorldEditorDoc::~CWorldEditorDoc()
+{
+}
+
+BOOL CWorldEditorDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CWorldEditorDoc 序列化
+
+void CWorldEditorDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CWorldEditorDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CWorldEditorDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:  strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CWorldEditorDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = NULL;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != NULL)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CWorldEditorDoc 诊断
+
+#ifdef _DEBUG
+void CWorldEditorDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CWorldEditorDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CWorldEditorDoc 命令
